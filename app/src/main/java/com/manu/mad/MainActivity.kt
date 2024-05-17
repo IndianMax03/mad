77436a11077d44a0cca28b13a1af6ef27c66b661
IndianMax03 <vermamanu@mail.ru>
2024-05-17 03:36:08 +0300

    Init: jetpack compose application

---
+++ app/src/main/java/com/manu/mad/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.manu.mad
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+        }
+    }
+}
+
